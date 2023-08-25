561691690a918346df32b2a4ac3451f57b19f423
Yaswanth <kandimalla.yaswanth@tifin.com>
2023-08-25 13:03:54 +0530

    typo update

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,8 +1,7 @@
-from dotenv import load_dotenv
 import os
 def load_config():
     return {
         "DB_USER": os.getenv("MONGO_DB_USER"),
-        "DB_PASSWORD": os.getenv("MONDO_DB_PASSWORD"),
+        "DB_PASSWORD": os.getenv("MONGO_DB_PASSWORD"),
         "JIRA_TOKEN":os.getenv("JIRA_TOKEN")
     }
