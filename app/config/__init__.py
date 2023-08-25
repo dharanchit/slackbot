761dd574cdb2d2f979968c9bc5dc4b246b870b7a
Yaswanth <kandimalla.yaswanth@tifin.com>
2023-08-25 13:00:22 +0530

    created env file

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,6 +1,8 @@
+from dotenv import load_dotenv
+import os
 def load_config():
     return {
-        "DB_USER": "dharanchit",
-        "DB_PASSWORD": "8dy23BTn8uQrYD3F",
-        "JIRA_TOKEN":"ATATT3xFfGF0v4BCob_Tj3cYykZ3O9HgkWjhf_RZwdJR3n1utEanefo4HB7RIyOISE9LBVlN3RPvGx_buAONkDy6lZvdOnyl-sXsIaVBCS0yMHuTPTVOnQyW3_3PTH0ahdvRRLxu_nk6HmW85GfYBgUa7mXYb9RkT_Uhi2qBo7SGVbq-9NRbIlc=9C7D4D58"
+        "DB_USER": os.getenv("MONGO_DB_USER"),
+        "DB_PASSWORD": os.getenv("MONDO_DB_PASSWORD"),
+        "JIRA_TOKEN":os.getenv("JIRA_TOKEN")
     }
